a5c38c8dd150fb48e1b4c30adf7d0322f667ecd3
Vasily Pleshakov <vp515c@att.com>
2015-07-16 14:41:27 -0500

    output only node number

--- fuel/getnodes.sh
+++ fuel/getnodes.sh
@@ -23,9 +23,9 @@ if [ " $DEBUG " = true ]; then
 echo " env $env"
 echo " Role $role"
 
-echo " fuel node --list $env | awk -F '|' -v r= $role 'NR>2 && \$ 7~r {print \$ 0}' "
+echo " fuel node --list $env | awk -F '|' -v r= $role 'NR>2 && \$ 7~r {print \$ 1}' "
 echo
 echo
 fi
-fuel node --list $env 2 >/dev/null | awk -F ' | ' -v r= $role ' NR>2 &&$7~r {print $0} '
+fuel node --list $env 2 >/dev/null | awk -F ' | ' -v r= $role ' NR>2 &&$7~r {print $1} '
 
