bc682cba6827e8cab714c1b56f7dad8f445f3a50
Vasily Pleshakov <vp515c@att.com>
2015-07-16 14:58:53 -0500

    fix syntax in getnodes.sh

--- fuel/getnodes.sh
+++ fuel/getnodes.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-DEBUG =false
-env =""
-role =" .* "
-while getopts " e:r: " arg; do
+DEBUG=false
+env=""
+role=".*"
+while getopts "e:r:" arg; do
   case $arg in
     e )
-      env =" --env $OPTARG"
+      env="--env $OPTARG"
       ;;
     r )
-      role =" $OPTARG "
+      role="$OPTARG"
       ;;
     * )
-      echo " Usage:
+      echo "Usage:
 $0 [-e <env>] [-r role]"
       exit 1
       ;;
   esac
 done
 
-if [ " $DEBUG " = true ]; then
-echo " env $env"
-echo " Role $role"
-
-echo " fuel node --list $env | awk -F '|' -v r= $role 'NR>2 && \$ 7~r {print \$ 1}' "
-echo
-echo
+if [ "$DEBUG" = true ]; then
+  echo "env $env"
+  echo "Role $role"
+  echo "fuel node --list $env | awk -F '|' -v r=$role 'NR>2 && \$ 7~r {print \$ 1}'"
+  echo
+  echo
 fi
-fuel node --list $env 2 >/dev/null | awk -F ' | ' -v r= $role ' NR>2 &&$7~r {print $1} '
+
+fuel node --list $env 2 >/dev/null | awk -F '|' -v r=$role 'NR>2 &&$7~r {print $1}'
 
